91db932d34542732032bf681435f169f931fdd6b
James McMorran <jtmcmorran@gmail.com>
2021-07-04 19:48:14 -0400

    route controller finished

---
+++ controllers/routeController.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const viewController = require('./viewController');
+const dataController = require('./datacontroller');
+router.get('/', dataController.index,viewController.index)
+router.delete('/:id', dataController.destroy, viewController.reHome)
+router.put('/:id', dataController.update, viewController.reShow)
+router.post('/', dataController.create, viewController.reHome)
+router.get('/:id/edit', dataController.show, viewController.edit)
+router.get('/:id', dataController.show, viewController.show)
+
+module.exports=router;
--- server.js
+++ server.js
@@ -4,17 +4,21 @@ const methodOverride = require('method-override');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const db = require('./models/db');
+//set up database
 db.once('connected',()=>{
   console.log('connected to mongo')
 })
+//view engine
 app.set('view engine','jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+//middleware and controllers
 app.use((req,res,next)=>{
   res.locals.data = {}
   next()
 });
 app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'));
+app.use(express.static('public'));
 app.use('/store',require('./controllers/routeController'));
 app.listen(PORT, ()=>{
   console.log('listening on port ', PORT);
